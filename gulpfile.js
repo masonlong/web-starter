f10f10d252e740919f291e4bf5680bc96fdcc93c
Peter D'Orsi <github@sinsound.com>
2014-07-09 15:34:36 -0700

    Gulp performance tweaks

--- gulpfile.js
+++ gulpfile.js
@@ -1,19 +1,21 @@
 /* global require*/
 'use strict';
 
-var gulp   = require('gulp'),
-    args   = require('yargs').argv,
-    gutil  = require('gulp-util'),
-    jshint = require('gulp-jshint'),
-    sass   = require('gulp-sass'),
-    uglify = require('gulp-uglify'),
-    gulpif = require('gulp-if'),
-    concat = require('gulp-concat'),
-    rename = require('gulp-rename'),
-    prefix = require('gulp-autoprefixer'),
+var gulp     = require('gulp'),
+    args     = require('yargs').argv,
+    gutil    = require('gulp-util'),
+    jshint   = require('gulp-jshint'),
+    sass     = require('gulp-sass'),
+    uglify   = require('gulp-uglify'),
+    gulpif   = require('gulp-if'),
+    concat   = require('gulp-concat'),
+    rename   = require('gulp-rename'),
+    prefix   = require('gulp-autoprefixer'),
     imagemin = require('gulp-imagemin'),
-    cache = require('gulp-cache'),
-    neat   = require('node-neat').includePaths;
+    changed  = require('gulp-changed'),
+    cache    = require('gulp-cache'),
+    neat     = require('node-neat').includePaths,
+    runSequence = require('run-sequence');
 
 var paths = {
     scss: './assets/sass/*.scss',
@@ -23,7 +25,6 @@ var paths = {
 
 var AUTOPREFIXER_BROWSERS = [
     'ie >= 10',
-    'ie_mob >= 10',
     'ff >= 30',
     'chrome >= 34',
     'safari >= 7',
@@ -57,11 +58,12 @@ gulp.task('lint', function() {
 // Covert/Compress Sass
 gulp.task('styles', function() {
     return gulp.src(paths.scss)
+        .pipe(changed('./app/css/'))
         .pipe(sass({
             outputStyle: isProduction ? 'compressed' : 'expanded',
             includePaths: ['styles'].concat(neat)
         }))
-        .pipe(prefix('last 2 versions'))
+        .pipe(prefix(AUTOPREFIXER_BROWSERS))
         .pipe(gulpif(isProduction, rename({suffix: '.min'})))
         .pipe(gulp.dest('./app/css'));
 });
@@ -80,5 +82,8 @@ gulp.task('watch', function() {
     gulp.watch(paths.js, ['scripts']);
 });
 
-gulp.task('default', ['styles', 'lint', 'scripts']);
+gulp.task('default', ['lint'], function (cb) {
+    runSequence('styles', ['scripts'], cb);
+});
+
 gulp.task('images', ['images']);
